6e12bf4ca709d8252055103dbbd53d12429e30dd
Christian  Cruz <christiancruz@MacBook-Air-de-Christian.local>
2020-12-05 19:52:32 -0500

    update serve socket

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+//App server
+const app = express();
+
+const path = require('path');
+require('dotenv').config();
+
+//Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+
+
+//Path pùblico 
+const publicPath = path.resolve(__dirname,'public');
+
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT,(err)=>{
+    if(err) throw new Error(err);
+
+    console.log(`Servidor corriendo en el puerto!!!`,process.env.PORT);
+});
